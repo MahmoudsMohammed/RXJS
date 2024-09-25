652de6f7880aab6742dd9dc9b6586264aa321cd2
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-09-25 07:31:00 +0300

    display all corses based on there category

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { finalize, map, shareReplay } from "rxjs/operators";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  http$: Observable<Course[]> = Observable.create((sub) => {
+    fetch("/api/courses")
+      .then((res) => res.json())
+      .then((data) => {
+        sub.next(data);
+        sub.complete();
+      })
+      .catch((err) => sub.error(err));
+  }).pipe(
+    map((data) => {
+      return data["payload"];
+    }),
+    finalize(() => console.log("the request finish")),
+    shareReplay()
+  );
 
+  Beginner$: Observable<Course[]> = this.http$.pipe(
+    map((data) => data.filter((e) => e.category === "BEGINNER"))
+  );
+  ADVANCED$: Observable<Course[]> = this.http$.pipe(
+    map((data) => data.filter((e) => e.category === "ADVANCED"))
+  );
 
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+  ngOnInit() {}
 }
